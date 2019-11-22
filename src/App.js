78dd51b215b77e045e6a64eb2ec66d351fc4c584
aruppert <aruppert@pm.me>
2019-11-22 13:53:36 +0100

    Deleted unused link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import Header from "./components/Header";
-import MovieList from "./components/MovieList";
 
 function App() {
   return (
